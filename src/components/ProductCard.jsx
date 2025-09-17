58289e7946cf2f776074357521c2e1d783ae1678
LeaGonz <leag1001@hotmail.com>
2025-09-17 17:27:52 +0100

    feat(card products): add quantity selector

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,22 @@
-import { CirclePlus } from "lucide-react"
+import { CirclePlus, Minus, Plus } from "lucide-react"
 import { formatCurrency } from "../utils/currencyFormat"
+import { useState } from "react"
 
 export default function ProductCard({ product, addToCart }) {
+
+    const [quantity, setQuantity] = useState(1)
 
     const nameInitials = product.name
         .split(" ")
         .map(word => word[0].toUpperCase())
         .join("")
+
+    const selectQuantity = (n) => {
+        const newQuantity = Math.max(1, Math.min(20, quantity + n))
+        setQuantity(newQuantity)
+    }
+
+    const mmButtons = "flex items-center justify-center w-7 h-7 rounded-full bg-gray-100 active:scale-95 transition-all duration-150 hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed"
 
     return (
         <div className="bg-white/90 rounded-2xl shadow-sm p-2 flex items-center gap-3">
@@ -33,26 +43,60 @@ export default function ProductCard({ product, addToCart }) {
                 </div>
             </div>
 
+            {/* Quantity */}
+            {Number.isFinite(product.price) && product.price > 0 && (
+                <div className="flex items-center gap-3">
+                    <button
+                        className={mmButtons}
+                        disabled={quantity <= 1}
+                        onClick={() => selectQuantity(-1)}
+                    >
+                        <Minus className="w-4 h-4" />
+                    </button>
+
+                    <div className="flex items-center justify-center">
+                        <span className="text-sm font-semibold text-gray-900">{quantity}</span>
+                    </div>
+
+                    <button
+                        className={mmButtons}
+                        disabled={quantity >= 20}
+                        onClick={() => selectQuantity(1)}
+
+                    >
+                        <Plus className="w-4 h-4" />
+                    </button>
+                </div>
+            )}
+
             {/* Price */}
-            <div className="w-24 text-right">
+            <div className="text-right">
+                {quantity > 1 &&
+                    <div className="text-xs text-gray-500">
+                        {quantity}x {formatCurrency(product.price)}
+                    </div>
+                }
                 <div className={`text-sm font-semibold ${product.price ? "text-green-700" : "text-gray-400"}`}>
-                    {formatCurrency(product.price)}
+                    {formatCurrency(product.price * quantity)}
                 </div>
             </div>
 
             {/* Add button */}
-            {(Number.isFinite(product.price) && product.price > 0) &&
+            {Number.isFinite(product.price) && product.price > 0 && (
                 <div className="flex items-center">
                     <button
-                        className="bg-white active:scale-95"
+                        className="bg-white active:scale-95 transition-all duration-150"
                         aria-label={`Add ${product.name}`}
                         style={{ WebkitTapHighlightColor: "transparent" }}
-                        onClick={() => addToCart(product)}
+                        onClick={() => {
+                            addToCart(product, quantity)
+                            setQuantity(1)  // reset default quantity
+                        }}
                     >
                         <CirclePlus className="w-6 h-6 text-green-600" />
                     </button>
                 </div>
-            }
+            )}
         </div >
     )
 }
